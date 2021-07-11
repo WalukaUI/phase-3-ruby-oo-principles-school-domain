c0b6fe0e5e0cf9064ffb36a7d47988080b34008e
WalukaUI <cwaluka@yahoo.com>
2021-07-10 17:09:18 -0700

    completed

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster={}
+    end
+    def add_student(stu_name,grade)
+       roster[grade]  ||= []
+       roster[grade] << stu_name
+    end
+    def grade(stu_grade)
+     roster[stu_grade]
+    end
+    def sort
+      sorted={}
+       roster.each do |num,stu|
+       sorted[num]=stu.sort
+      end
+      sorted
+    end
+end
